51b5531619f848e180ac1f0c967eaf34bda2595b
absentees <scottblissett@gmail.com>
2017-02-17 10:35:04 +1100

    poop

--- theme-src/js/main.js
+++ theme-src/js/main.js
@@ -1,11 +1,8 @@
 /* globals WebFont */
 /* jshint unused:false */
 
-import BigText from 'BigText';
-
 $(document).ready(function () {
 	if ($('.bigtext').length) {
-		const bigText = new BigText();
-		bigText.init();
+		$('.bigtext').bigtext();
 	}
 });

5c2cd2afcb8ca0d8fd453acb3354aae496d0e59d
Scott <scott@pollen.com.au>
2019-08-15 09:13:37 +1000

    Rewrite with gridsome

---
+++ gridsome.config.js
@@ -0,0 +1,20 @@
+// This is where project configuration and plugin options are located. 
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Good Internet',
+  plugins: [
+    {
+      use: '@gridsome/source-airtable',
+      options: {
+        apiKey: process.env.AIRTABLE_API_KEY, // required
+        baseId: process.env.AIRTABLE_BASE_ID, // required
+        tableName: 'Good', // required
+        typeName: 'Site' // required
+      }
+    }
+  ]
+}
